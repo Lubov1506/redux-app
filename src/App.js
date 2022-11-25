32ec3b53ef8e8c8cda4f71e42f55e7f356c82dc5
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-11-25 18:24:17 +0200

    redux count

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
 import './App.css';
-import { useReducer } from 'react';
+import { Component, useReducer } from 'react';
+import { connect } from 'react-redux';
 
-function reducer (state, action){
-  switch(action.type){
-    case 'PLUS_ONE':{
-      return {}
-    }
-    case 'SUB_ONE':{
-      return {}
-    }
-  }
-  return state
-}
-
-function App () {
-  const [state, dispatch] = useReducer(reducer, {
-    counr: 0
-  });
-
+const App = (props) => {
+  const {count, dispatch} = props
   const increment = () => {
-    dispatch({
-      type: 'PLUS_ONE',
-      count: state.count+1
-    })
+    const action = {
+      type: 'INCREMENT_COUNT'
+    }
+    dispatch(action)
   };
   const decrement = () => {
-    dispatch({
-      type: 'SUB_ONE',
-      count: state.count-1
-    })
+    const action = {
+      type: 'DECREMENT_COUNT'
+    }
+    dispatch(action)
   };
+
   return (
     <div className='App'>
-      <h1>{state.count}</h1>
+      <h1>{count}</h1>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
     </div>
   );
 }
-
-export default App;
+const mapStateToProps = (state) =>{
+  return {
+    count: state.count
+  }
+}
+const WrappedApp = connect(mapStateToProps)(App);
+export default WrappedApp;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,38 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import {Provider} from 'react-redux'
+
+const initState = {
+  count: 0
+}
+const reducer = (state = initState, action) =>{
+  switch(action.type){
+    case 'INCREMENT_COUNT':{
+      return {
+        ...state,
+        count: state.count+1
+      }
+    }
+    case 'DECREMENT_COUNT':{
+      return {
+        ...state,
+        count: state.count-1
+      }
+    }
+  }
+  return {...state}
+}
+
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
