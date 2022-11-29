6d5b97a85c0e1d3cc812e1b2c29202361e7eee44
Lubov Parfyonova <lubaparfyonova@gmail.com>
2022-11-29 13:34:13 +0200

    userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,35 +1,39 @@
 import ACTION_TYPES from '../actions/actionTypes';
 
 const initState = {
-    users:[],
-    error: null,
-    isFetching: false
-}
+  users: [],
+  error: null,
+  isFetching: false
+};
 
 const userReducer = (state = initState, action) => {
-  switch(action.type){
-    case ACTION_TYPES.CREATE_USER_REQUEST :{
-
-        return {
-            ...state
-        }
+  switch (action.type) {
+    case ACTION_TYPES.CREATE_USER_REQUEST: {
+      return {
+        ...state,
+        isFetching: true
+      };
     }
-    case ACTION_TYPES.CREATE_USER_SUCCESS :{
-
-        return{
-            ...state
-        }
+    case ACTION_TYPES.CREATE_USER_SUCCESS: {
+      const { values } = action;
+      const { users } = state;
+      return {
+        ...state,
+        users: [...users, values],
+        isFetching: false
+      };
     }
-    case ACTION_TYPES.CREATE_USER_ERROR :{
-
-        return{
-            ...state
-        }
+    case ACTION_TYPES.CREATE_USER_ERROR: {
+      const { error } = action;
+      return {
+        ...state,
+        isFetching: false,
+        eroor: error
+      };
     }
-    default:{
-        return state
-    } 
-
+    default: {
+      return state;
+    }
   }
 };
 export default userReducer;
